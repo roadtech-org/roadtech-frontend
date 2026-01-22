f0bd63098377a44d7864d8e997344ad7457c4d98
Abhay Kumar Vishwakarma <akraj6938@gmail.com>
2026-01-22 18:27:22 +0530

    icon size fix

--- vite.config.ts
+++ vite.config.ts
@@ -22,11 +22,11 @@ export default defineConfig({
         icons: [
           {
             src: 'pwa-192x192.png',
-            sizes: '192x192',
+            sizes: '384x384',
             type: 'image/png'
           },
           {
-            src: 'pwa-192x192.png',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
